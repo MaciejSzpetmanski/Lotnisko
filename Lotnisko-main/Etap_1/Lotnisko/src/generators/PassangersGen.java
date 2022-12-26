bf8f65c01b4890d4787c88b6edda9e6bc10ea7c4
AgEwa <agata@osmalek.pl>
2022-12-26 15:29:10 +0100

    sprawdzam commitowanie z intelliJ'a

--- Lotnisko-main/Etap_1/Lotnisko/src/generators/PassangersGen.java
+++ Lotnisko-main/Etap_1/Lotnisko/src/generators/PassangersGen.java
@@ -1,6 +1,9 @@
 package generators;
 
-import passangers.*;
+import passangers.BusinessPassanger;
+import passangers.Passanger;
+import passangers.Person;
+import passangers.VIPPassanger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,6 +11,9 @@ import java.util.Random;
 
 public class PassangersGen {
     public static List<Person> generate(int howMany){
+        // generuje n pasażerów o różnych typach z pawdopodobieństwem 0.1-VIP
+        //0.9*0.3-Business i reszte naormalny
+        // brakuje ścieżek i odlotów;
         Random random = new Random();
         List<Person> ListOfPeople = new ArrayList<>();
         for(int i=0;i<howMany;i++){
